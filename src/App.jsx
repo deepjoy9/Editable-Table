93b5e3ef1530aa049a344fbcac9ed0c5a7478fbc
deepjoy9 <deepjoysarkar9@gmail.com>
2024-07-12 20:54:43 +0530

    Created an editable table application
    - Added Table component with JSX and CSS for displaying and managing table data
    - Added Modal component with JSX and CSS for form input and modal display
    - Added App.jsx with state management and integration of Table and Modal components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
 import "./App.css";
+import { Table } from "./components/Table/Table";
+import { Modal } from "./components/Modal/Modal";
 
 function App() {
-  return <>Editable Table</>;
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      page: "Home",
+      description: "This is the main page of the website",
+      status: "live",
+    },
+    {
+      page: "About Us",
+      description: "This page has details about the company",
+      status: "draft",
+    },
+    {
+      page: "Pricing",
+      description: "Prices for different subscriptions",
+      status: "error",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
+  return (
+    <div className="App">
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
+      </button>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
